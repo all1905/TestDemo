67a2e7044e2604da3663eb657f283ee9e8717167
Aleksandr <cameramnet1@gmail.com>
2018-12-09 00:12:20 +0200

    class PairOfDigits chenge PairFx

--- src/main/java/ArabicToRomanMumberConverter.java
+++ src/main/java/ArabicToRomanMumberConverter.java
@@ -12,56 +12,21 @@ public class ArabicToRomanMumberConverter {
         String romanNumber = getRepitingRomanNumber(arabicNumber);
             return romanNumber;
         }
-    public class Pair<K, V> {
 
-        private final K Key;
-        private final V Value;
-
-        public  <K, V> Pair<K, V> createPair(K Key, V Value) {
-            return new Pair<K, V>(Key, Value);
-        }
-
-        public Pair(K Key, V Value) {
-            this.Key = Key;
-            this.Value = Value;
-        }
-
-        public K getKey() {
-            return Key;
-        }
-
-        public V getValue() {
-            return Value;
-        }
-
-    }
 
     private String getRepitingRomanNumber(int arabicNumber) {
         String romanNumber = "";
-        Pair<Integer,String> one= new Pair<Integer, String>(1,"I");
-         Pair<Integer,String> five = new Pair<Integer, String>(5,"V");
-        ArrayList<Pair<Integer,String>> map =new ArrayList<Pair<Integer, String>>();
-        map.add(five);
-        map.add(one);
+        ArrayList<PairOfDigits> digits =new ArrayList<PairOfDigits>();
+        digits.add(new PairOfDigits(5,'V'));
+        digits.add(new PairOfDigits(1,'I'));
 
-        for(Pair<Integer,String> digit :map){
-            while (arabicNumber >= digit.getKey()) {
-                romanNumber += digit.getValue();
-                arabicNumber-=digit.getKey();
+        for(PairOfDigits digit :digits){
+            while (arabicNumber >= digit.getArabic()) {
+                romanNumber += digit.getRomen();
+                arabicNumber-=digit.getArabic();
             }
         }
 
-
-
-
-//        while (arabicNumber >= 5) {
-//            romanNumber += "V";
-//            arabicNumber-=5;
-//        }
-//        while (arabicNumber >= 1) {
-//            romanNumber += "I";
-//            arabicNumber--;
-//        }
         return romanNumber;
     }
 
---
+++ src/main/java/PairOfDigits.java
@@ -0,0 +1,17 @@
+public class PairOfDigits {
+    private  int arabic;
+    private  char romen;
+
+    public PairOfDigits(int arabic, char romen) {
+        this.arabic = arabic;
+        this.romen = romen;
+    }
+
+    public int getArabic() {
+        return arabic;
+    }
+
+    public char getRomen() {
+        return romen;
+    }
+}
